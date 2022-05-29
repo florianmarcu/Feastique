da0e3acf38200ebecb12cc34b9b28f6607c76242
florianmarcu <florian.marcu23@gmail.com>
2022-05-29 16:35:40 +0300

    Updated Discover Page

--- lib/screens/home_page/components/discover_page/discover_page.dart
+++ lib/screens/home_page/components/discover_page/discover_page.dart
@@ -1,3 +1,4 @@
+import 'components/theme.dart';
 import 'package:flutter/material.dart';
 
 class DiscoverPage extends StatelessWidget {
@@ -5,18 +6,34 @@ class DiscoverPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height,
-      child: Center(
-        child: TextButton(
-          onPressed: (){},
-          child: Container(
-            height: 150,
-            width: MediaQuery.of(context).size.width*0.8,
-            child: Center(child: Text("Găsește localul perfect"))
+    return Theme(
+      data: _buildTheme(context),
+      child: Container(
+        height: MediaQuery.of(context).size.height,
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 140),
+              Text(
+                "Ai rămas fără idei?",
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              SizedBox(height: 12,),
+              TextButton( /// 'Find the perfect place' button
+                onPressed: (){},
+                child: Container(
+                  height: 150,
+                  width: MediaQuery.of(context).size.width*0.8,
+                  child: Center(child: Text("Găsește localul perfect"))
+                ),
+              ),
+            ],
           ),
-        )
+        ),
       ),
     );
   }
+
+  ThemeData _buildTheme(BuildContext context) => Theme.of(context).copyWith(textButtonTheme: textButtonTheme(context));
 }
