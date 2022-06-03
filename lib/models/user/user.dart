c8835f1af71fc651b06e0290b098aa391a0494d9
florianmarcu <florian.marcu23@gmail.com>
2022-06-03 23:25:56 +0300

    Updated User model

--- lib/models/user/user.dart
+++ lib/models/user/user.dart
@@ -2,7 +2,7 @@ import 'package:authentication/authentication.dart';
 
 class UserProfile{
   
-  final String? uid;
+  final String uid;
   final String? email;
   final String? photoURL;
   final String? displayName;
@@ -10,23 +10,23 @@ class UserProfile{
   String? phoneNumber;
   bool? isManager;
 
-  UserProfile({this.uid,this.email,this.photoURL, this.displayName, this.isAnonymous, this.phoneNumber});
+  UserProfile(this.uid, {this.email,this.photoURL, this.displayName, this.isAnonymous, this.phoneNumber});
 }
 
 /// Converts a User to UserProfile
 UserProfile? userToUserProfile(User? user){
   return user != null
   ? UserProfile(
-    uid: user.uid,
-      email: user.email,
-      photoURL: user.photoURL,
-      phoneNumber: user.phoneNumber,
-      displayName: user.displayName != null 
-        ? user.displayName 
-        : (user.email != null
-          ? user.email!.substring(0,user.email!.indexOf('@'))
-          : "Guest"),
-      isAnonymous : user.isAnonymous
+    user.uid,
+    email: user.email,
+    photoURL: user.photoURL,
+    phoneNumber: user.phoneNumber,
+    displayName: user.displayName != null 
+      ? user.displayName 
+      : (user.email != null
+        ? user.email!.substring(0,user.email!.indexOf('@'))
+        : "Guest"),
+    isAnonymous : user.isAnonymous
   )
   : null;
 }
