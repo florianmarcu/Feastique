4c5959a3a34083c2b5376181a4cba56cd15b7633
florianmarcu <florian.marcu23@gmail.com>
2022-06-03 23:24:46 +0300

    Added a 'constants' file

---
+++ lib/config/constants.dart
@@ -0,0 +1,23 @@
+const Map<String, List<String>> kFilters = {
+  "types": [
+    "burger",
+    "tea",
+    "asian",
+    "italian",
+    "pizza",
+    "greek",
+    "sushi",
+    "coffee",
+    "beer"
+  ],
+  "ambiances": [
+    "calm", 
+    "social-friendly"
+  ],
+  "costs":[
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+};
