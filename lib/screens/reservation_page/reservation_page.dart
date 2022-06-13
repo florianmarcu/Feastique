0d7c1cc20d1c9916a3d94aa5e9e59908dd6d06b3
florianmarcu <florian.marcu23@gmail.com>
2022-06-13 11:15:55 +0300

    AddedAReservationPageForEachReservation

--- lib/screens/reservation_page/reservation_page.dart
+++ lib/screens/reservation_page/reservation_page.dart
@@ -2,14 +2,33 @@ import 'package:feastique/screens/reservation_page/reservation_provider.dart';
 import 'package:flutter/material.dart';
 
 class ReservationPage extends StatelessWidget {
-  const ReservationPage({ Key? key }) : super(key: key);
+
+  final _scrollController = ScrollController(
+    keepScrollOffset: true,
+    initialScrollOffset: 0
+  );
 
   @override
   Widget build(BuildContext context) {
     var provider = context.watch<ReservationProvider>();
     return Scaffold(
-      body: Center(
-        child: Text(provider.reservation.id)
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        //toolbarHeight: 70,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.only(bottomLeft: Radius.elliptical(210, 30), bottomRight: Radius.elliptical(210, 30))),
+        title: Center(child: Text("Rezervare confirmată", style: Theme.of(context).textTheme.headline4,)),
+        // bottom: PreferredSize(
+        //   preferredSize: Size(MediaQuery.of(context).size.width, 80),
+        //   child: Row(children: [
+            
+        //   ],)
+        // ),
+      ),
+      body: CustomScrollView(
+        controller: _scrollController,
+        slivers: [
+          Text(provider.reservation.id),
+        ],
       )
     );
   }
