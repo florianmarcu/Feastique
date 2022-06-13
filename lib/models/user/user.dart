64fb672d01240ab343d507bfa499c78f03e46fa0
florianmarcu <florian.marcu23@gmail.com>
2022-06-13 11:16:52 +0300

    UpdatedUserModelSoItCanFetchFurherDataFromDB

--- lib/models/user/user.dart
+++ lib/models/user/user.dart
@@ -1,4 +1,5 @@
 import 'package:authentication/authentication.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class UserProfile{
   
@@ -8,26 +9,43 @@ class UserProfile{
   final String? displayName;
   final bool? isAnonymous;
   String? phoneNumber;
-  bool? isManager;
+  Future<bool>? isManager;
 
   UserProfile(this.uid, {this.email,this.photoURL, this.displayName, this.isAnonymous, this.phoneNumber});
 }
 
 /// Converts a User to UserProfile
 UserProfile? userToUserProfile(User? user){
-  return user != null
-  ? UserProfile(
-    user.uid,
-    email: user.email,
-    photoURL: user.photoURL,
-    phoneNumber: user.phoneNumber,
-    displayName: user.displayName != null 
-      ? user.displayName 
-      : (user.email != null
-        ? user.email!.substring(0,user.email!.indexOf('@'))
-        : "Guest"),
-    isAnonymous : user.isAnonymous
-  )
-  : null;
+  if(user != null){
+
+    var userProfile = UserProfile(
+      user.uid,
+      email: user.email,
+      photoURL: user.photoURL,
+      phoneNumber: user.phoneNumber,
+      displayName: user.displayName != null 
+        ? user.displayName 
+        : (user.email != null
+          ? user.email!.substring(0,user.email!.indexOf('@'))
+          : "Guest"),
+      isAnonymous : user.isAnonymous,
+    );
+
+    _fetchExtraData(userProfile);
+    
+    return userProfile;
+
+  }
+  else return null;
 }
 
+void _fetchExtraData(UserProfile userProfile){
+  bool isManager = false;
+
+  FirebaseFirestore.instance.collection('users').doc(userProfile.uid).get()
+  .then((doc){
+    isManager = doc.data()!.containsKey('isManager') && doc.data()!['isManager'] == true;
+
+  });
+  //userProfile.isManager
+}
