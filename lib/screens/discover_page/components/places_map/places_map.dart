62b85dd7303c21b47dccf0ad69a19884268c6cee
florianmarcu <florian.marcu23@gmail.com>
2022-06-23 12:31:15 +0300

    MapStartsAtCity'sLocation

--- lib/screens/discover_page/components/places_map/places_map.dart
+++ lib/screens/discover_page/components/places_map/places_map.dart
@@ -1,4 +1,5 @@
 import 'package:feastique/screens/discover_page/discover_provider.dart';
+import 'package:feastique/screens/wrapper_home_page/wrapper_home_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -9,11 +10,15 @@ class PlacesMap extends StatelessWidget {
   Widget build(BuildContext context) {
     var places = context.watch<DiscoverPageProvider>().places;
     var markers = context.watch<DiscoverPageProvider>().markers;
+    var wrapperHomePageProvider = context.watch<WrapperHomePageProvider>();
+    var mainCity = wrapperHomePageProvider.mainCity;
     return Container(
       child: GoogleMap(
-        padding: EdgeInsets.only(top: 200),
+        padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
         myLocationEnabled: false,
-        initialCameraPosition: CameraPosition(target: LatLng(44.427466, 26.102500), zoom: 14),
+        initialCameraPosition: mainCity != null ?
+        CameraPosition(target: LatLng(mainCity['location'].latitude, mainCity['location'].longitude), zoom: 14)
+        : CameraPosition(target: LatLng(0,0)),
         markers: markers,
       ),
       height: MediaQuery.of(context).size.height,
