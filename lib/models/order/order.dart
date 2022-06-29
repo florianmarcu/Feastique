ac74ac371d153b9f682f431e8f7a285a2f68baf3
florianmarcu <florian.marcu23@gmail.com>
2022-06-30 00:41:17 +0300

    UpdatedOrderModel

--- lib/models/order/order.dart
+++ lib/models/order/order.dart
@@ -1,21 +1,25 @@
 import 'package:feastique/models/models.dart';
 
 class Order{
+  String id;
   Reservation reservation;
   List items;
   DateTime dateCreated;
   String details;
   bool? accepted;
+  bool canceled;
 
-  Order({required this.reservation, required this.items, required this.dateCreated, required this.details, required this.accepted});
+  Order({required this.id, required this.reservation, required this.items, required this.dateCreated, required this.details, required this.accepted, required this.canceled});
 }
 
-  Order orderDataToOrder(Reservation reservation, Map<String, dynamic> data){
+  Order orderDataToOrder(Reservation reservation, String id,  Map<String, dynamic> data){
     return Order(
+      id: id, 
       reservation: reservation,
       items: data['items'],
       dateCreated: DateTime.fromMillisecondsSinceEpoch(data['date_created'].millisecondsSinceEpoch),
       details: data['details'],
-      accepted: data['accepted']
+      accepted: data['accepted'],
+      canceled: data['canceled'] != true ? false : true
     );
   }
