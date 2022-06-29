e1bc53058b4aef37c43e0d42eb06ee56757602ea
florianmarcu <florian.marcu23@gmail.com>
2022-06-30 00:38:30 +0300

    RefactoredManagerHomePage

--- lib/screens/manager_home_page/manager_home_page.dart
+++ lib/screens/manager_home_page/manager_home_page.dart
@@ -1,56 +1,21 @@
-import 'package:feastique/config/config.dart';
-import 'package:feastique/screens/manager_orders_page/manager_orders_page.dart';
+import 'package:feastique/screens/manager_home_page/manager_home_provider.dart';
 import 'package:feastique/screens/manager_orders_page/manager_orders_provider.dart';
-import 'package:feastique/screens/manager_reservations_page/manager_reservations_page.dart';
 import 'package:feastique/screens/manager_reservations_page/manager_reservations_provider.dart';
 import 'package:flutter/material.dart';
 
-class ManagerHomePage extends StatefulWidget {
+class ManagerHomePage extends StatelessWidget {
 
-  @override
-  State<ManagerHomePage> createState() => _ManagerHomePageState();
-}
-
-class _ManagerHomePageState extends State<ManagerHomePage> {
-  int _selectedScreenIndex = 0;
-
-  List<Widget> _screens = <Widget>[
-    ChangeNotifierProvider<ManagerReservationsProvider>(
-      create: (context) => ManagerReservationsProvider(context),
-      builder: (context, _) {
-        return ManagerReservationsPage();
-      }
-    ),
-    ChangeNotifierProvider<ManagerOrdersProvider>(
-      create: (context) => ManagerOrdersProvider(context),
-      builder: (context, _) {
-        return ManagerOrdersPage();
-      }
-    ),
-  ];
-
-  List<BottomNavigationBarItem> _screenLabels = <BottomNavigationBarItem>[
-    BottomNavigationBarItem(
-      label: "Rezervări",
-      icon: Image.asset(localAsset("reservation"), width: 20,)
-    ),
-    BottomNavigationBarItem(
-      label: "Comenzi",
-      icon: Image.asset(localAsset("orders"), width: 20,)
-    ),
-  ];
 
   @override
   Widget build(BuildContext context) {
-
-
+    var provider = context.watch<ManagerHomePageProvider>();
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
         automaticallyImplyLeading: false,
         //toolbarHeight: 70,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.only(bottomLeft: Radius.elliptical(210, 30), bottomRight: Radius.elliptical(210, 30))),
-        title: Center(child: Text(_screenLabels[_selectedScreenIndex].label!, style: Theme.of(context).textTheme.headline4,)),
+        title: Center(child: Text(provider.screenLabels[provider.selectedScreenIndex].label!, style: Theme.of(context).textTheme.headline4,)),
         // bottom: PreferredSize(
         //   preferredSize: Size(MediaQuery.of(context).size.width, 80),
         //   child: Row(children: [
@@ -60,15 +25,25 @@ class _ManagerHomePageState extends State<ManagerHomePage> {
       ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
-        currentIndex: _selectedScreenIndex,
-        items: _screenLabels,
-        onTap: (index) => setState(()=>_selectedScreenIndex = index),
+        currentIndex: provider.selectedScreenIndex,
+        items: provider.screenLabels,
+        onTap: (index) => 
+        // provider.updateSelectedScreenIndex(index),
+        provider.pageController.animateToPage(index, duration: Duration(milliseconds: 200), curve: Curves.easeIn)
       ),
-      body: Center(
-        child: IndexedStack(
-          children: _screens,
-          index: _selectedScreenIndex
-        )
+      // body: Center(
+      //   child: IndexedStack(
+      //     children: _screens,
+      //     index: provider.selectedScreenIndex
+      //   )
+      // ),
+      body: SizedBox.expand(
+        child: PageView(
+          physics: NeverScrollableScrollPhysics(),
+          children: provider.screens,
+          controller: provider.pageController,
+          onPageChanged: (index) => provider.updateSelectedScreenIndex(index),
+        ),
       ),
     );
   }
--- lib/screens/manager_home_page/manager_home_provider.dart
+++ lib/screens/manager_home_page/manager_home_provider.dart
@@ -1,5 +1,42 @@
+import 'package:feastique/config/config.dart';
+import 'package:feastique/screens/manager_orders_page/manager_orders_page.dart';
+import 'package:feastique/screens/manager_orders_page/manager_orders_provider.dart';
+import 'package:feastique/screens/manager_reservations_page/manager_reservations_page.dart';
+import 'package:feastique/screens/manager_reservations_page/manager_reservations_provider.dart';
 import 'package:flutter/material.dart';
 
 class ManagerHomePageProvider with ChangeNotifier{
+  int selectedScreenIndex = 0;
+  PageController pageController = PageController(initialPage: 0);
   
+  List<Widget> screens = <Widget>[
+    ChangeNotifierProvider<ManagerReservationsPageProvider>(
+      create: (context) => ManagerReservationsPageProvider(context),
+      builder: (context, _) {
+        return ManagerReservationsPage();
+      }
+    ),
+    ChangeNotifierProvider<ManagerOrdersPageProvider>(
+      create: (context) => ManagerOrdersPageProvider(context),
+      builder: (context, _) {
+        return ManagerOrdersPage();
+      }
+    ),
+  ];
+
+  List<BottomNavigationBarItem> screenLabels = <BottomNavigationBarItem>[
+    BottomNavigationBarItem(
+      label: "Rezervări",
+      icon: Image.asset(localAsset("reservation"), width: 20,)
+    ),
+    BottomNavigationBarItem(
+      label: "Comenzi",
+      icon: Image.asset(localAsset("orders"), width: 20,)
+    ),
+  ];
+  void updateSelectedScreenIndex(int index){
+    selectedScreenIndex = index;
+
+    notifyListeners();
+  }
 }
