7d166deeb157f90088e6d754f0c724c822cde077
florianmarcu <florian.marcu23@gmail.com>
2022-06-30 00:41:07 +0300

    UpdatedLocalAssetMethod

--- lib/config/paths.dart
+++ lib/config/paths.dart
@@ -2,69 +2,7 @@
 import 'dart:typed_data';
 /// Returns the full asset path for a specific file
 String localAsset(String fileName){
-  switch(fileName){
-    case 'social-friendly':
-      return "assets/icons/social-friendly.png";
-    case 'calm':
-      return "assets/icons/intimate.png";
-    case 'filter':
-      return "assets/icons/filter.png";
-    case 'list':
-      return "assets/icons/list.png";
-    case 'no-results-found':
-      return "assets/icons/no-results-found.png";
-    case 'map':
-      return "assets/icons/map.png";
-    case 'cuisine':
-      return "assets/icons/cuisine.png";
-    case 'edit':
-      return "assets/icons/edit.png";
-    case 'time':
-      return "assets/icons/time.png";
-    case 'calendar':
-      return "assets/icons/calendar.png";
-    case 'user':
-      return "assets/icons/user.png";
-    case 'detail':
-      return "assets/icons/detail.png";
-    case 'phone':
-      return "assets/icons/phone.png";
-    case 'place':
-      return "assets/icons/place.png";
-    case 'question':
-      return "assets/icons/question.png";
-    case 'reservation':
-      return "assets/icons/reservation.png";
-    case 'orders':
-      return "assets/icons/orders.png";
-    case 'google':
-      return "assets/icons/google.png";
-    case 'facebook':
-      return "assets/icons/facebook.png";
-    case 'apple':
-      return "assets/icons/apple.png";
-    case 'pin':
-      return "assets/icons/pin.png";
-    case 'pizza':
-      return "assets/icons/pizza.png";
-    case 'italian':
-      return "assets/icons/italian.png";
-    case 'beer':
-      return "assets/icons/beer.png";
-    case 'coffee':
-      return "assets/icons/coffee.png";
-    case 'european':
-      return "assets/icons/european.png";
-    case 'wine':
-      return "assets/icons/wine.png";  
-    case 'accepted':
-      return "assets/icons/accepted.png";
-    case 'refused':
-      return "assets/icons/refused.png";
-    case 'claimed':
-      return "assets/icons/claimed.png";    
-    default: return "";
-  }
+  return "assets/icons/$fileName.png";
 }
 
 Uint8List cloudAsset(String fileName){
