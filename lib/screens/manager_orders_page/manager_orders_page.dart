18420cc7f937d1bc977cba2c80b76c0e6b902b1a
florianmarcu <florian.marcu23@gmail.com>
2022-06-30 00:39:38 +0300

    FinishedManagerOrders

--- lib/screens/manager_orders_page/manager_orders_page.dart
+++ lib/screens/manager_orders_page/manager_orders_page.dart
@@ -1,92 +1,159 @@
+import 'package:feastique/config/config.dart';
+import 'package:feastique/screens/manager_orders_page/components/order_item_page.dart';
 import 'package:feastique/screens/manager_orders_page/manager_orders_provider.dart';
 import 'package:flutter/material.dart';
 
 class ManagerOrdersPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var provider = context.watch<ManagerOrdersProvider>();
+    var provider = context.watch<ManagerOrdersPageProvider>();
+    var orders = provider.orders;
     return  ScrollConfiguration(
-        behavior: ScrollBehavior(androidOverscrollIndicator: AndroidOverscrollIndicator.stretch),
-        child: ListView.builder(
-          shrinkWrap: true,
-          padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top + 20, left: 20, right: 20),
-          itemCount: provider.orders == null ? 1 : provider.orders!.length,
-          itemBuilder: (context, index){
+      behavior: ScrollBehavior(androidOverscrollIndicator: AndroidOverscrollIndicator.stretch),
+      child: ListView.builder(
+        shrinkWrap: true,
+        padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top + 20, left: 20, right: 20),
+        itemCount: orders == null ? 1 : orders.length,
+        itemBuilder: (context, index){
+          if(orders == null)
+            return Container(
+              padding: EdgeInsets.only(top: 95),
+              height: 100,
+              child: LinearProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(Theme.of(context).primaryColor), backgroundColor: Colors.transparent,)
+            );
+          // else if(orders == [])
+          //   return Center(
+          //     child: Column(
+          //       mainAxisSize: MainAxisSize.min,
+          //       children: [
+          //         Image.asset(
+          //           localAsset("no-results-found"),
+          //           color: Colors.black54,
+          //           width: 70,
+          //         ),
+          //         SizedBox(height: 20,),
+          //         Text("Nu există comenzi :(", style: Theme.of(context).textTheme.headline6!.copyWith(fontSize: 20, color: Theme.of(context).textTheme.headline6!.color!.withOpacity(0.54)))
+          //       ],
+          //     ),
+          //   );
+          else {
+            var order = orders[index];
+            var totalCount = 0;
+            for(int i = 0; i < order.items.length; i++){
+              totalCount += int.tryParse(order.items[i]['count'].toString())!;
+            }
             return Column(
               children: [
                 ClipRRect(
                   borderRadius: BorderRadius.circular(20),
-                  child: Container(
-                    color: Theme.of(context).highlightColor,
-                    //padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                    height: 100,
-                    child: Row(
-                      children: [
-                        SizedBox(width: 20,),
-                        // FutureProvider<Image?>.value(
-                        //   value: provider.getImage(reservation.placeId),
-                        //   initialData: null,
-                        //   builder: (context, child){
-                        //     var image = Provider.of<Image?>(context);
-                        //     return SizedBox(
-                        //       width: 100,
-                        //       height: 100,
-                        //       child: image != null 
-                        //       ? AspectRatio(
-                        //         aspectRatio: 1.5,
-                        //         child: image,
-                        //       )
-                        //       : CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(Theme.of(context).primaryColor), backgroundColor: Colors.transparent,)
-                        //     );
-                        //   },
-                        // ),
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text("Comandă nouă la 22:50", style: Theme.of(context).textTheme.labelMedium),
-                              // Row(
-                              //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              //   children: [
-                              //     Text.rich( /// The Date
-                              //       TextSpan(
-                              //         children: [
-                              //           WidgetSpan(child: Image.asset(asset('calendar'), width: 18)),
-                              //           WidgetSpan(child: SizedBox(width: 10)),
-                              //           TextSpan(
-                              //             text: formatDateToDay(reservation.dateStart)
-                              //           ),
-                              //         ]
-                              //       )
-                              //     ),
-                              //     Text.rich( /// The Time 
-                              //       TextSpan(
-                              //         children: [
-                              //           WidgetSpan(child: Image.asset(asset('time'), width: 18)),
-                              //           WidgetSpan(child: SizedBox(width: 10)),
-                              //           TextSpan(
-                              //             text: formatDateToHourAndMinutes(reservation.dateStart)
-                              //           ),
-                              //         ]
-                              //       )
-                              //     ),
-                              //   ],
-                              // ),
-                            ],
+                  child: MaterialButton(
+                    padding: EdgeInsets.zero,
+                    onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context)=>
+                      ChangeNotifierProvider.value(
+                        value: provider,
+                        child: OrderItemPage(order),
+                      )
+                    )).whenComplete(() => provider.getData()),
+                    child: Container(
+                      color: Theme.of(context).highlightColor,
+                      //padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                      height: 100,
+                      child: Row(
+                        children: [
+                          SizedBox(width: 20,),
+                          // FutureProvider<Image?>.value(
+                          //   value: provider.getImage(reservation.placeId),
+                          //   initialData: null,
+                          //   builder: (context, child){
+                          //     var image = Provider.of<Image?>(context);
+                          //     return SizedBox(
+                          //       width: 100,
+                          //       height: 100,
+                          //       child: image != null 
+                          //       ? AspectRatio(
+                          //         aspectRatio: 1.5,
+                          //         child: image,
+                          //       )
+                          //       : CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(Theme.of(context).primaryColor), backgroundColor: Colors.transparent,)
+                          //     );
+                          //   },
+                          // ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("Comandă nouă la ${formatDateToHourAndMinutes(order.dateCreated)}", style: Theme.of(context).textTheme.labelMedium),
+                                Row(
+                                  children: [
+                                    Text.rich( /// The User's name
+                                      TextSpan(
+                                        children: [
+                                          WidgetSpan(child: Icon(Icons.person_outline, size: 20)),
+                                          WidgetSpan(child: SizedBox(width: 10)),
+                                          TextSpan(
+                                            text: order.reservation.guestName != null 
+                                            ? order.reservation.guestName
+                                            : " - "
+                                          ),
+                                        ]
+                                      )
+                                    ),
+                                    SizedBox(width: 20,),
+                                    Text.rich( /// The User's name
+                                      TextSpan(
+                                        children: [
+                                          WidgetSpan(child: Image.asset(localAsset("item"), width: 22)),
+                                          WidgetSpan(child: SizedBox(width: 10)),
+                                          TextSpan(
+                                            text: "x $totalCount"
+                                          ),
+                                        ]
+                                      )
+                                    ),
+                                  ],
+                                ),                            // Row(
+                                //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                //   children: [
+                                //     Text.rich( /// The Date
+                                //       TextSpan(
+                                //         children: [
+                                //           WidgetSpan(child: Image.asset(asset('calendar'), width: 18)),
+                                //           WidgetSpan(child: SizedBox(width: 10)),
+                                //           TextSpan(
+                                //             text: formatDateToDay(reservation.dateStart)
+                                //           ),
+                                //         ]
+                                //       )
+                                //     ),
+                                //     Text.rich( /// The Time 
+                                //       TextSpan(
+                                //         children: [
+                                //           WidgetSpan(child: Image.asset(asset('time'), width: 18)),
+                                //           WidgetSpan(child: SizedBox(width: 10)),
+                                //           TextSpan(
+                                //             text: formatDateToHourAndMinutes(reservation.dateStart)
+                                //           ),
+                                //         ]
+                                //       )
+                                //     ),
+                                //   ],
+                                // ),
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
-                    )
+                        ],
+                      )
+                    ),
                   ),
                 ),
                 Container(height: 20,)
               ],
             );
           }
-        )
-      );
-    
+        }
+      )
+    );
   }
 }
--- lib/screens/manager_orders_page/manager_orders_provider.dart
+++ lib/screens/manager_orders_page/manager_orders_provider.dart
@@ -4,26 +4,95 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 export 'package:provider/provider.dart';
 
-class ManagerOrdersProvider with ChangeNotifier{
+class ManagerOrdersPageProvider with ChangeNotifier{
   List<Order>? orders;
   BuildContext context;
+  bool isLoading = false;
 
-  ManagerOrdersProvider(this.context){
+  ManagerOrdersPageProvider(this.context){
     getData();
   }
 
-  void getData() async{
+  Future<void> getData() async{
 
     var user = context.read<User?>();
 
     var place = (await FirebaseFirestore.instance.collection("users").doc(user!.uid).collection("managed_places").get()).docs.first;
 
-    var orders = [];
+    orders = [];
 
-    await FirebaseFirestore.instance.collectionGroup("reservations")
+    var query = await FirebaseFirestore.instance.collectionGroup("reservations")
     .where("active", isEqualTo: true)
     .where("place_id", isEqualTo: place.id)
     .get();
+
+    for(int i = 0; i < query.docs.length; i++){
+      var doc = query.docs[i];
+      var reservation = reservationDataToReservation(doc.id, doc.data());
+      if(!doc.reference.toString().contains("managed_places")){
+        var query = await doc.reference.collection("orders").get();
+        for(int i = 0; i < query.docs.length; i++){
+          var doc = query.docs[i];
+          print(doc.data().containsKey("accepted"));
+          if(doc.data()['accepted'] == null)
+            orders!.add(orderDataToOrder(reservation, doc.id, doc.data(),));
+        }
+      }
+    }
+    
+    notifyListeners();
   }
 
+  Future<void> acceptOrder(Order order) async{
+    _loading();
+    
+    await order.reservation.placeReservationRef!.collection("orders").doc(order.id)
+    .set(
+      {
+        "accepted" : true
+      },
+      SetOptions(merge: true)
+    );
+
+    await order.reservation.userReservationRef!.collection("orders").doc(order.id)
+    .set(
+      {
+        "accepted" : true
+      },
+      SetOptions(merge: true)
+    );
+    _loading();
+
+    notifyListeners();
+  }
+
+  Future<void> refuseOrder(Order order) async{
+    _loading();
+
+    await order.reservation.placeReservationRef!.collection("orders").doc(order.id)
+    .set(
+      {
+        "accepted" : true
+      },
+      SetOptions(merge: true)
+    );
+
+    await order.reservation.userReservationRef!.collection("orders").doc(order.id)
+    .set(
+      {
+        "accepted" : true
+      },
+      SetOptions(merge: true)
+    );
+
+    _loading();
+
+    notifyListeners();
+  }
+
+  void _loading(){
+    isLoading = !isLoading;
+
+    notifyListeners();
+  }
 }
